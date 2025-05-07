e8c4a56ffa852b7d290e2ea5ab3f78d433e184f6
Ralph Baesso <ralphsbaesso@gmail.com>
2025-05-07 14:01:04 -0300

    #1 - fix: update order_flow_items table

--- lib/generators/order_flow/install/templates/migrate/create_order_flow_items.rb
+++ lib/generators/order_flow/install/templates/migrate/create_order_flow_items.rb
@@ -3,7 +3,7 @@
 class CreateOrderFlowItems < ActiveRecord::Migration[6.1]
   def change
     create_table :order_flow_items do |t|
-      t.string :status
+      t.integer :status, null: false, default: 0
       t.jsonb :data, default: {}
       t.jsonb :attr, default: {}
       t.references :order_flow_order, null: false, foreign_key: true
